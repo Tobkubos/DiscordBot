e4b505c49e3a9a45e557e4977721ed63d8cc67fb
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2026-06-13 13:40:31 +0200

    bot connection

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿import dotenv from "dotenv";
+dotenv.config();
+
+import { Client, GatewayIntentBits, Events } from "discord.js";
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+client.once(Events.ClientReady, () => {
+	console.log(`Bot ready: ${client.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+	if (message.author.bot) return;
+	console.log(`Message from ${message.author.tag}: ${message.content}`);
+});
+
+console.log("Wczytany token:", process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
